e405153bad681306958a5c5879230fda8cedaad5
David Miller <david.r.miller@outlook.com>
2023-12-05 11:05:38 -0800

    day 5 part 2 completed

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
-use std::{collections::HashMap, fs};
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[command(
     author = "David Miller",
     version = "v1.0.0",
-    about = "Advent of Code (Day 4)"
+    about = "Advent of Code (Day 5)"
 )]
 
 struct Cli {
@@ -18,6 +18,7 @@ struct Span {
     start: i64,
     end: i64,
     offset: i64,
+    index: i64,
 }
 
 #[derive(Debug)]
@@ -27,7 +28,6 @@ struct ItemMap {
 
 impl Span {
     fn new(s: &str) -> Self {
-        // println!("creating new span from {}", s);
         let (dst, rem) = s.split_once(' ').unwrap();
         let (src, len) = rem.split_once(' ').unwrap();
 
@@ -35,7 +35,12 @@ impl Span {
         let end = start + len.parse::<i64>().unwrap();
         let offset = dst.parse::<i64>().unwrap() - start;
 
-        Span { start, end, offset }
+        Span {
+            start,
+            end,
+            offset,
+            index: 0,
+        }
     }
 
     fn contains(&self, n: i64) -> bool {
@@ -43,19 +48,26 @@ impl Span {
     }
 
     fn get(&self, n: i64) -> i64 {
-        // if !self.contains(n) {
-        //     return None;
-        // }
-
-        // Some(n + self.offset)
         n + self.offset
+    }
+}
+
+impl Iterator for Span {
+    type Item = i64;
+    fn next(&mut self) -> Option<i64> {
+        let out = self.start + self.index;
+        if out <= self.end {
+            self.index += 1;
+            Some(out)
+        } else {
+            None
+        }
     }
 }
 
 impl ItemMap {
     fn new(s: &str) -> Self {
         let mut spans = Vec::new();
-        // println!("creating new item map from {}", s);
         for row in s.split_terminator('\n') {
             if !row.contains(':') && !row.trim().is_empty() {
                 spans.push(Span::new(row))
@@ -63,14 +75,6 @@ impl ItemMap {
         }
         ItemMap { spans }
     }
-
-    // fn contains(&self, n: i64) -> bool {
-    //     self.spans.iter().any(|x| x.contains(n))
-    // }
-
-    // fn get(&self, n: i64) -> i64 {
-    //     self.spans.iter().fold(n, |acc, x| x.get(acc))
-    // }
 
     fn get(&self, n: i64) -> i64 {
         for s in self.spans.iter() {
@@ -82,42 +86,82 @@ impl ItemMap {
     }
 }
 
-fn parse_input(s: &str) -> (Vec<i64>, Vec<ItemMap>) {
+fn parse_seeds_p1(s: &str) -> Vec<i64> {
     let mut seeds = Vec::new();
-    let mut maps = Vec::new();
-
-    let (seeds_str, rem) = s.split_once('\n').unwrap();
-    let (_, seed_nums) = seeds_str.split_once(' ').unwrap();
+    let (_, seed_nums) = s.split_once(' ').unwrap();
     for n in seed_nums.split(' ') {
         seeds.push(n.parse::<i64>().unwrap());
     }
+    seeds
+}
 
-    for map_str in rem.split("\n\n") {
+fn parse_seeds_p2(s: &str) -> Vec<Span> {
+    let mut seeds = Vec::new();
+    let (_, seed_nums) = s.split_once(' ').unwrap();
+
+    let mut start = 0;
+    let mut end = 0;
+    for n in seed_nums.split(' ') {
+        let num = n.parse::<i64>().unwrap();
+        if start == 0 {
+            start = num;
+        } else {
+            end = start + num - 1;
+        }
+        if start != 0 && end != 0 {
+            seeds.push(Span {
+                start,
+                end,
+                offset: 0,
+                index: 0,
+            });
+            start = 0;
+            end = 0;
+        }
+    }
+
+    seeds
+}
+
+fn parse_item_map(s: &str) -> Vec<ItemMap> {
+    let mut maps = Vec::new();
+
+    for map_str in s.split("\n\n") {
         if !map_str.trim().is_empty() {
-            let imap = ItemMap::new(map_str);
-            println!("New Item Map: {:?}", imap);
             maps.push(ItemMap::new(map_str));
         }
     }
 
-    (seeds, maps)
+    maps
 }
 
 fn solve_part1(s: &str) -> i64 {
-    let (seeds, maps) = parse_input(s);
-    let mut starting_locs = Vec::new();
-    for seed in seeds {
-        starting_locs.push((seed, maps.iter().fold(seed, |acc, x| x.get(acc))));
-    }
-    println!("starting locations: {:?}", starting_locs);
-    let min = starting_locs
+    let (seeds_str, rem) = s.split_once('\n').unwrap();
+    let seeds = parse_seeds_p1(seeds_str);
+    let maps = parse_item_map(rem);
+
+    seeds
         .into_iter()
-        .reduce(|acc, e| if e.1 < acc.1 { e } else { acc });
-    min.unwrap().1
+        .map(|s| maps.iter().fold(s, |acc, x| x.get(acc)))
+        .min()
+        .unwrap()
 }
 
-fn solve_part2(s: &str) -> usize {
-    0
+fn solve_part2(s: &str) -> i64 {
+    let (seeds_str, rem) = s.split_once('\n').unwrap();
+    let seeds = parse_seeds_p2(seeds_str);
+    let maps = parse_item_map(rem);
+
+    seeds
+        .into_iter()
+        .map(|span| {
+            span.into_iter()
+                .map(|s| maps.iter().fold(s, |acc, x| x.get(acc)))
+                .min()
+                .unwrap()
+        })
+        .min()
+        .unwrap()
 }
 
 fn main() {
