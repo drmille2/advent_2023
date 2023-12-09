4b71a1b3a58c08d309f003f167644de7a6196987
David Miller <david.r.miller@outlook.com>
2023-12-09 12:23:39 -0800

    refactor for part 2

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::{fs, rc::Rc};
+use std::{collections::VecDeque, fs};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -13,34 +13,47 @@ struct Cli {
     input: String,
 }
 
-type Series = Vec<isize>;
+type Series = VecDeque<isize>;
 
-fn calc_deriv(x: &[isize]) -> Series {
-    let mut out = Vec::new();
+fn calc_deriv(x: &VecDeque<isize>) -> Series {
+    let mut out = VecDeque::new();
     for n in 1..x.len() {
-        out.push(x[n] - x[n - 1]);
+        out.push_back(x[n] - x[n - 1]);
     }
     out
 }
 
 fn rextend_series(x: &mut Series) {
-    if x.windows(2).all(|x| x[0] == x[1]) {
-        x.push(x[0]);
+    // if x.into_iter().windows(2).all(|x| x[0] == x[1]) {
+    if x.iter().all(|a| a == &x[0]) {
+        x.push_back(x[0]);
         return;
     }
 
     let mut d = calc_deriv(x);
     rextend_series(&mut d);
-    let next = x.last().unwrap() + *d.last().unwrap();
-    x.push(next);
+    let next = *x.iter().last().unwrap() + d.into_iter().last().unwrap();
+    x.push_back(next);
 }
+
+// fn lextend_series(x: &mut Series) {
+//     if x.windows(2).all(|x| x[0] == x[1]) {
+//         x.push(x[0]);
+//         return;
+//     }
+
+//     let mut d = calc_deriv(x);
+//     lextend_series(&mut d);
+//     let next = x.last().unwrap() + *d.last().unwrap();
+//     x.push(next);
+// }
 
 fn parse_input(s: &str) -> Vec<Series> {
     let mut out = Vec::new();
     for line in s.split_terminator('\n') {
-        let mut series = Vec::new();
+        let mut series = VecDeque::new();
         for num in line.split(' ') {
-            series.push(num.parse::<isize>().unwrap());
+            series.push_back(num.parse::<isize>().unwrap());
         }
         out.push(series);
     }
@@ -52,7 +65,7 @@ fn solve_part1(s: &str) -> isize {
     let mut out = 0;
     for mut series in all_series {
         rextend_series(&mut series);
-        out += series.last().unwrap();
+        out += series.into_iter().last().unwrap();
     }
     // let mut series = all_series[2].clone();
     // println!("series {:?}", series);
